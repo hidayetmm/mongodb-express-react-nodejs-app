003361349913a4676e5437c365b9ca6a0ec17200
Hidayat <hidayetmmdff@gmail.com>
2021-10-24 14:10:49 +0400

    users/exercises can be added

---
+++ backend/routes/users.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+let User = require("../models/user.model");
+
+router.route("/").get((req, res) => {
+  User.find()
+    .then((users) => res.json(users))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/add").post((req, res) => {
+  const username = req.body.username;
+  const newUser = new User({ username });
+
+  newUser
+    .save()
+    .then((successData) => {
+      const resData = {
+        data: successData,
+        message: "User added!",
+        result: "success",
+      };
+      res.send(resData);
+    })
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+module.exports = router;
